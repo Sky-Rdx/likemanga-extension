84aa8fc068de8aadefaf10e032e176d6d604985b
Sky-Rdx <familygroup197@gmail.com>
2026-02-21 21:11:53 +0530

    Create build.gradle.kts

---
+++ likemangaorg/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = "eu.kanade.tachiyomi.extension.en.likemangaorg"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "eu.kanade.tachiyomi.extension.en.likemangaorg"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+        debug {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation("eu.kanade.tachiyomi:multisrc:1.0")
+}
